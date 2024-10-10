6d3d2269fc0461b0975cb3af1067ba7abd913b35
Rayan <rayanhldr@gmail.com>
2024-10-10 22:14:30 +0530

    Objectives Section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import NavBar from "@/components/NavBar";
 import Image from "next/image";
 import Footer from "@/components/Footer"
+import NavBar from "@/components/NavBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
