a336cc8c27b84072114e88ea9aeca5199e4b51e9
Rayan <rayanhldr@gmail.com>
2024-10-12 02:59:19 +0530

    Color and changes due to design changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavBar() {
 	return (
-		<div className={'flex bg-white flex-row shadow-md shadow-gray-400 py-5 px-12 rounded-[40px] *:px-12 text-lg'}>
+		<div className={'flex bg-white flex-row  py-5 px-12 rounded-[40px] *:px-12 text-lg'}>
 			<div>About Us</div>
 			<Link href="/events">Events</Link>
 			<div>Yuva Sangam</div>
