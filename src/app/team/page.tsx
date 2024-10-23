cc33d13db72785b67861d4de295eae8713dab70f
Rayan <rayanhldr@gmail.com>
2024-10-23 15:31:43 +0530

    Team page kinda mobile responsive

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Card from "@/components/team/Card";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Page() {
 	const [isFullTeam, setIsFullTeam] = useState(true)
@@ -12,13 +12,17 @@ export default function Page() {
 	const [isMultimedia, setIsMultimedia] = useState(true)
 	const [isWeb, setIsWeb] = useState(true)
 
+
+	const [dropValue, setDropValue] = useState("Full Team")
+	const [showDropdown, setShowDropdown] = useState(true)
+
 	return (
 		<div className="flex flex-col">
-			<div className="flex flex-col mb-44 *:mx-auto text-background bg-foreground rounded-b-[4rem] lg:rounded-b-[8rem] pt-[2rem] relative" >
+			<div className="flex flex-col mb-16 lg:mb-44 *:mx-auto text-background bg-foreground rounded-b-[3rem] lg:rounded-b-[8rem] pt-[2rem] relative" >
 				<div className="flex flex-col *:mx-auto mt-24 lg:mt-52">
 					<div className="text-lg mb-2">Meet the incredible</div>
 					<div className="text-[2rem] lg:text-[3.4rem] font-extrabold text-center leading-none mb-4">TEAM EBSB<br />2024-25</div>
-					<div className="text-center w-[30rem] lg:w-[36rem] py-8 leading-snug mb-48">Get to know the talented individuals who form the backbone of our organization. Each member brings a unique set of skills, experiences, and passion, contributing to the synergy that drives our success.</div>
+					<div className="text-center px-6 lg:px-auto lg:w-[36rem] py-4 lg:py-8 leading-snug mb-24 lg:mb-48">Get to know the talented individuals who form the backbone of our organization. Each member brings a unique set of skills, experiences, and passion, contributing to the synergy that drives our success.</div>
 				</div>
 				<img
 					src="/watermark.svg"
@@ -26,7 +30,7 @@ export default function Page() {
 				<img
 					src="/watermark.svg"
 					className="w-fit right-0 absolute scale-x-[-1]" />
-				<div className="absolute self-center flex flex-row bottom-[-2rem] bg-white text-foreground text-xl px-6 py-6 rounded-[3rem] drop-shadow-lg *:mx-8">
+				<div className="absolute self-center hidden lg:flex flex-row bottom-[-2rem] bg-white text-foreground text-xl px-6 py-6 rounded-[3rem] drop-shadow-lg *:mx-8">
 					<div data-isFullTeam={isFullTeam} onClick={() => { setIsFullTeam(true); setIsPR(true); setIsFinance(true); setIsDesign(true); setIsWeb(true); setIsMultimedia(true); setIsEvents(true) }} className="data-[isFullTeam=true]:font-bold">Full Team</div>
 					<div data-isPr={isPR && !isFullTeam} onClick={() => { setIsFullTeam(false); setIsPR(true); setIsFinance(false); setIsDesign(false); setIsWeb(false); setIsMultimedia(false); setIsEvents(false) }} className="data-[isPr=true]:font-bold">PR & Networking</div>
 					<div data-isEvents={isEvents && !isFullTeam} onClick={() => { setIsFullTeam(false); setIsPR(false); setIsFinance(false); setIsDesign(false); setIsWeb(false); setIsMultimedia(false); setIsEvents(true) }} className="data-[isEvents=true]:font-bold">Events</div>
@@ -37,18 +41,37 @@ export default function Page() {
 				</div>
 			</div>
 
+			<div data-dropdown={showDropdown} className="data-[dropdown=true]:bg-foreground data-[dropdown=true]:text-background data-[dropdown=true]:rounded-b-[0] relative lg:hidden flex flex-row px-6 py-2 mx-6 mb-14 border-foreground border-solid border-2 rounded-[3rem] " >
+				<div className="grow text-center" onClick={() => setShowDropdown(!showDropdown)}>{dropValue}</div>
+				<img src="/team/dropdown.svg" onClick={() => setShowDropdown(true)} className="justify-self-center" />
+				<div data-toshow={showDropdown} className="data-[toshow=true]:block hidden bg-foreground text-background absolute top-12 left-0 *:my-1 *:py-2 w-full rounded-b-[2rem] py-6 px-8 text-center " >
+					<div onClick={() => { setShowDropdown(false); setDropValue("Full Team"); setIsFullTeam(true); setIsPR(true); setIsFinance(true); setIsDesign(true); setIsWeb(true); setIsMultimedia(true); setIsEvents(true) }}>Full Team</div>
+					<hr />
+					<div onClick={() => { setShowDropdown(false); setDropValue("PR & Networking"); setIsFullTeam(false); setIsPR(true); setIsFinance(false); setIsDesign(false); setIsWeb(false); setIsMultimedia(false); setIsEvents(false) }}>PR & Networking</div>
+					<hr />
+					<div onClick={() => { setShowDropdown(false); setDropValue("Finance & Equipment"); setIsFullTeam(false); setIsPR(false); setIsFinance(true); setIsDesign(false); setIsWeb(false); setIsMultimedia(false); setIsEvents(false) }}>Finance & Equipment</div>
+					<hr />
+					<div onClick={() => { setShowDropdown(false); setDropValue("Events"); setIsFullTeam(false); setIsPR(false); setIsFinance(false); setIsDesign(false); setIsWeb(false); setIsMultimedia(false); setIsEvents(true) }}>Events</div>
+					<hr />
+					<div onClick={() => { setShowDropdown(false); setDropValue("Design"); setIsFullTeam(false); setIsPR(false); setIsFinance(false); setIsDesign(true); setIsWeb(false); setIsMultimedia(false); setIsEvents(false) }}>Design</div>
+					<hr />
+					<div onClick={() => { setShowDropdown(false); setDropValue("Multimedia"); setIsFullTeam(false); setIsPR(false); setIsFinance(false); setIsDesign(false); setIsWeb(false); setIsMultimedia(true); setIsEvents(false) }}>Multimedia</div>
+					<hr />
+					<div onClick={() => { setShowDropdown(false); setDropValue("Web"); setIsFullTeam(false); setIsPR(false); setIsFinance(false); setIsDesign(false); setIsWeb(true); setIsMultimedia(false); setIsEvents(false) }}>Web</div>
+				</div>
+			</div>
 
 			<div data-isFullTeam={isFullTeam} className="flex data-[isFullTeam=false]:hidden flex-row w-full justify-evenly mb-16">
 				<Card src="/team/santoshi.png" title="OVERALL HEAD" name="Santoshi Gaytri Mavuru" />
 				<Card src="/team/tanvisree.png" title="MENTOR" name="Tanvisree Nimma" />
 			</div>
 
-			<div data-isPR={isPR} className="flex flex-col data-[isPR=false]:hidden justify-items-center mb-24 *:mx-auto">
-				<div className="text-[3rem] font-bold">PR & NETWORKING</div>
+			<div data-isPR={isPR} className="flex flex-col data-[isPR=false]:hidden justify-items-center mb-16 lg:mb-24 *:mx-auto">
+				<div className="text-[2rem] lg:text-[3rem] font-bold">PR & NETWORKING</div>
 				<div className="text-lg mb-6">The art of crafting & sparking social buzz</div>
 				<div className="flex flex-col justify-items-center *:mx-auto">
 					<Card src="/team/pr/mayank.png" title="PR & NETWORKING HEAD" name="Mayank Gupta" />
-					<div className="flex flex-row mt-12 *:mx-4">
+					<div className="flex flex-row flex-wrap justify-center mt-12 *:mb-2 lg:*:mb-auto *:mx-2 lg:*:mx-4">
 						<Card src="/team/pr/venkata.png" title="" name="Venkata Dheeraj Kurapati" />
 						<Card src="/team/pr/sankeerth.png" title="" name="Sankeerth Kulakarni" />
 						<Card src="/team/pr/swathi.png" title="" name="Swathi Deepika" />
@@ -60,12 +83,12 @@ export default function Page() {
 
 
 
-			<div data-isFinance={isFinance} className="flex flex-col data-[isFinance=false]:hidden justify-items-center mb-24 *:mx-auto">
-				<div className="text-[3rem] font-bold">FINANCE & EQUIPMENT</div>
+			<div data-isFinance={isFinance} className="flex flex-col data-[isFinance=false]:hidden justify-items-center mb-16 lg:mb-24 *:mx-auto">
+				<div className="text-[2rem] lg:text-[3rem] font-bold">FINANCE & EQUIPMENT</div>
 				<div className="text-lg mb-6">The art of crafting & sparking social buzz</div>
 				<div className="flex flex-col justify-items-center *:mx-auto">
 					<Card src="/team/finance/naman.png" title="FINANCE & EQUIPMENT HEAD" name="Naman Preet Singh" />
-					<div className="flex flex-row mt-12 *:mx-4">
+					<div className="flex flex-row flex-wrap justify-center mt-12 *:mb-2 lg:*:mb-auto *:mx-2 lg:*:mx-4">
 						<Card src="/team/finance/garvit.png" title="" name="Garvit Maheshwari" />
 						<Card src="/team/finance/jeshanth.png" title="" name="Jeshanth Madda" />
 						<Card src="/team/finance/neel.png" title="" name="Neel Prajapati" />
@@ -76,12 +99,12 @@ export default function Page() {
 			</div>
 
 
-			<div data-isEvents={isEvents} className="flex flex-col data-[isEvents=false]:hidden justify-items-center mb-24 *:mx-auto">
-				<div className="text-[3rem] font-bold">EVENTS</div>
+			<div data-isEvents={isEvents} className="flex flex-col data-[isEvents=false]:hidden justify-items-center mb-16 lg:mb-24 *:mx-auto">
+				<div className="text-[2rem] lg:text-[3rem] font-bold">EVENTS</div>
 				<div className="text-lg mb-6">The art of crafting & sparking social buzz</div>
 				<div className="flex flex-col justify-items-center *:mx-auto">
 					<Card src="/team/events/yash.png" title="EVENTS HEAD" name="Yash Sonawane" />
-					<div className="flex flex-row mt-12 *:mx-4">
+					<div className="flex flex-row flex-wrap justify-center mt-12 *:mb-2 lg:*:mb-auto *:mx-2 lg:*:mx-4">
 						<Card src="/team/events/karike.png" title="" name="Karike Purvi" />
 						<Card src="/team/events/vishista.png" title="" name="Vishista Reddy" />
 						<Card src="/team/events/pratyus.png" title="" name="Pratyus Kumar Panda" />
@@ -92,12 +115,12 @@ export default function Page() {
 			</div>
 
 
-			<div data-isDesign={isDesign} className="flex flex-col data-[isDesign=false]:hidden justify-items-center mb-24 *:mx-auto">
-				<div className="text-[3rem] font-bold">DESIGN</div>
+			<div data-isDesign={isDesign} className="flex flex-col data-[isDesign=false]:hidden justify-items-center mb-16 lg:mb-24 *:mx-auto">
+				<div className="text-[2rem] lg:text-[3rem] font-bold">DESIGN</div>
 				<div className="text-lg mb-6">The art of crafting & sparking social buzz</div>
 				<div className="flex flex-col justify-items-center *:mx-auto">
 					<Card src="/team/design/manasi.png" title="DESIGN HEAD" name="Manasi Pasekar" />
-					<div className="flex flex-row mt-12 *:mx-4">
+					<div className="flex flex-row flex-wrap justify-center mt-12 *:mb-2 lg:*:mb-auto *:mx-2 lg:*:mx-4">
 						<Card src="/team/design/rishi.png" title="" name="Rishi Sangade" />
 						<Card src="/team/design/kishore.png" title="" name="Kishore j K" />
 						<Card src="/team/design/shreevel.png" title="" name="Shreevel K J" />
@@ -105,12 +128,12 @@ export default function Page() {
 				</div>
 			</div>
 
-			<div data-isMultimedia={isMultimedia} className="flex flex-col data-[isMultimedia=false]:hidden justify-items-center mb-24 *:mx-auto">
-				<div className="text-[3rem] font-bold">MULTIMEDIA</div>
+			<div data-isMultimedia={isMultimedia} className="flex flex-col data-[isMultimedia=false]:hidden justify-items-center mb-16 lg:mb-24 *:mx-auto">
+				<div className="text-[2rem] lg:text-[3rem] font-bold">MULTIMEDIA</div>
 				<div className="text-lg mb-6">The art of crafting & sparking social buzz</div>
 				<div className="flex flex-col justify-items-center *:mx-auto">
 					<Card src="/team/multimedia/pritam.png" title="MULTIMEDIA HEAD" name="Pritam Kumar Mandal" />
-					<div className="flex flex-row mt-12 *:mx-4">
+					<div className="flex flex-row flex-wrap justify-center mt-12 *:mb-2 lg:*:mb-auto *:mx-2 lg:*:mx-4">
 						<Card src="/team/multimedia/sasa.png" title="" name="Sasa Mardi" />
 						<Card src="/team/multimedia/viraj.png" title="" name="Viraj Mohan Rajpure" />
 						<Card src="/team/multimedia/vaishnav.png" title="" name="Vaishnav Dhruv" />
@@ -118,12 +141,12 @@ export default function Page() {
 				</div>
 			</div>
 
-			<div data-isWeb={isWeb} className="flex flex-col data-[isWeb=false]:hidden justify-items-center mb-24 *:mx-auto">
-				<div className="text-[3rem] font-bold">WEB</div>
+			<div data-isWeb={isWeb} className="flex flex-col data-[isWeb=false]:hidden justify-items-center mb-2 lg:mb-24 *:mx-auto">
+				<div className="text-[2rem] lg:text-[3rem] font-bold">WEB</div>
 				<div className="text-lg mb-6">The art of crafting & sparking social buzz</div>
 				<div className="flex flex-col justify-items-center *:mx-auto">
 					<Card src="/team/web/anand.png" title="WEB HEAD" name="Anand Kasyup" />
-					<div className="flex flex-row mt-12 *:mx-4">
+					<div className="flex flex-row flex-wrap justify-center mt-12 *:mb-2 lg:*:mb-auto *:mx-4">
 						<Card src="/team/web/bolla.png" title="" name="Bolla Lokesh Reddy" />
 						<Card src="/team/web/rayan.png" title="" name="Rayan Halder" />
 						<Card src="/team/web/ruthwik.png" title="" name="Ruthwik Kanna" />
@@ -131,8 +154,6 @@ export default function Page() {
 					</div>
 				</div>
 			</div>
-
-
 		</div >
 	)
 }
--- src/components/team/Card.tsx
+++ src/components/team/Card.tsx
@@ -4,11 +4,11 @@ export default function Card({ src, title, name }: { src: string, title: string,
 		toRenderTitle = false
 	}
 	return (
-		<div className="flex flex-col text-center w-fit h-fit">
+		<div className="flex flex-col text-center w-min lg:w-fit h-fit">
 
-			<img data-toRenderTitle={toRenderTitle} className="rounded-2xl mb-4 data-[toRenderTitle=true]:w-[15rem] data-[toRenderTitle=true]:h-[15rem] data-[toRenderTitle=false]:w-[13rem] data-[toRenderTitle=false]:h-[13rem]" src={src} />
+			<img data-toRenderTitle={toRenderTitle} className="rounded-2xl mb-4 data-[toRenderTitle=true]:min-w-[11rem] lg:data-[toRenderTitle=true]:w-[15rem] data-[toRenderTitle=true]:h-[11rem] lg:data-[toRenderTitle=true]:h-[15rem] data-[toRenderTitle=false]:min-w-[7rem]  lg:data-[toRenderTitle=false]:min-w-[13rem] data-[toRenderTitle=false]:h-[7rem] lg:data-[toRenderTitle=false]:h-[13rem]" src={src} />
 			<div data-toRenderTitle={toRenderTitle} className="data-[toRenderTitle=false]:hidden font-semibold text-lg">{title}</div>
-			<div>{name}</div>
+			<div className="text-wrap leading-snug">{name}</div>
 		</div>
 	)
 }
