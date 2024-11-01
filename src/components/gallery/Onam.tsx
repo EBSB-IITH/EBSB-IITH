ef31f2ae85296cf9f0f64cf9ee60ed4d86bf1e43
Anand <ep22btech11012@iith.ac.in>
2024-11-01 15:51:11 +0530

    debug commit -3 (revert)

--- src/components/gallery/Onam.tsx
+++ src/components/gallery/Onam.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image"
 import { headingStyle, innerDivStyle, outerDivStyle } from "./eventGalleryStyle"
 
 export function OnamGallery24() {
@@ -6,41 +5,41 @@ return (
 		<div className={outerDivStyle}>
 			<div className={headingStyle}>ONAM '24</div>
 			<div className={innerDivStyle}>
-				<div>
-					<Image fill alt="" src="onam/24/1.jpg" />
+				<div >
+					<img src="onam/24/1.jpg" />
 				</div>
 				<div>
-					<Image fill alt="" src="onam/24/2.jpg" />
+					<img src="onam/24/2.jpg" />
 				</div>
 				<div>
-					<Image fill alt="" src="onam/24/3.jpg" />
+					<img src="onam/24/3.jpg" />
 				</div>
 				<div>
-					<Image fill alt="" src="onam/24/4.jpg" />
+					<img src="onam/24/4.jpg" />
 				</div>
 				<div>
-					<Image fill alt="" src="onam/24/5.jpg" />
+					<img src="onam/24/5.jpg" />
 				</div>
 				<div>
-					<Image fill alt="" src="onam/24/6.jpg" />
+					<img src="onam/24/6.jpg" />
 				</div>
 				<div>
-					<Image fill alt="" src="onam/24/7.jpg" />
+					<img src="onam/24/7.jpg" />
 				</div>
 				<div>
-					<Image fill alt=""  src="onam/24/8.jpg" />
+					<img src="onam/24/8.jpg" />
 				</div>
 				<div>
-					<Image fill alt="" src="onam/24/9.jpg" />
+					<img src="onam/24/9.jpg" />
 				</div>
 				<div>
-					<Image fill alt="" src="onam/24/10.jpg" />
+					<img src="onam/24/10.jpg" />
 				</div>
 				<div>
-					<Image fill alt="" src="onam/24/11.jpg" />
+					<img src="onam/24/11.jpg" />
 				</div>
 				<div>
-					<Image fill alt="" src="onam/24/12.jpg" />
+					<img src="onam/24/12.jpg" />
 				</div>
 			</div>
 		</div >
