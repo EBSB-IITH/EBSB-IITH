5435bd3cb1d064a55b9cea4d4558f7a5d6f99380
Anand <ep22btech11012@iith.ac.in>
2024-11-01 17:13:26 +0530

    quick gallery debug

--- src/components/gallery/eventGalleryStyle.tsx
+++ src/components/gallery/eventGalleryStyle.tsx
@@ -1,5 +1,5 @@
 export const outerDivStyle = "flex flex-col py-6"
 export const headingStyle = "font-bold text-[2rem] text-center mb-8"
-export const innerDivStyle = "grid grid-rows-2 grid-flow-col h-[32rem] lg:h-[40rem] gap-4 px-12 lg:px-24 overflow-x-auto pb-12 scroll-mx-12 scrollbar-hidden w-[calc(27rem*6)]";
+export const innerDivStyle = "grid grid-rows-2 grid-flow-col h-[32rem] lg:h-[40rem] gap-4 px-12 lg:px-24 overflow-x-auto pb-12 scroll-mx-12 scrollbar-hidden auto-cols-[27rem]";
 
 
