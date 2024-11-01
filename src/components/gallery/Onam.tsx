5177de1e0f25353907c9bc2e6156f4d163437477
Anand <ep22btech11012@iith.ac.in>
2024-11-01 15:51:45 +0530

    debug 3.1

--- src/components/gallery/Onam.tsx
+++ src/components/gallery/Onam.tsx
@@ -6,40 +6,40 @@ return (
 			<div className={headingStyle}>ONAM '24</div>
 			<div className={innerDivStyle}>
 				<div >
-					<img src="onam/24/1.jpg" />
+					<img alt="" src="onam/24/1.jpg" />
 				</div>
 				<div>
-					<img src="onam/24/2.jpg" />
+					<img alt="" src="onam/24/2.jpg" />
 				</div>
 				<div>
-					<img src="onam/24/3.jpg" />
+					<img alt="" src="onam/24/3.jpg" />
 				</div>
 				<div>
-					<img src="onam/24/4.jpg" />
+					<img alt="" src="onam/24/4.jpg" />
 				</div>
 				<div>
-					<img src="onam/24/5.jpg" />
+					<img alt="" src="onam/24/5.jpg" />
 				</div>
 				<div>
-					<img src="onam/24/6.jpg" />
+					<img alt="" src="onam/24/6.jpg" />
 				</div>
 				<div>
-					<img src="onam/24/7.jpg" />
+					<img alt="" src="onam/24/7.jpg" />
 				</div>
 				<div>
-					<img src="onam/24/8.jpg" />
+					<img alt="" src="onam/24/8.jpg" />
 				</div>
 				<div>
-					<img src="onam/24/9.jpg" />
+					<img alt="" src="onam/24/9.jpg" />
 				</div>
 				<div>
-					<img src="onam/24/10.jpg" />
+					<img alt="" src="onam/24/10.jpg" />
 				</div>
 				<div>
-					<img src="onam/24/11.jpg" />
+					<img alt="" src="onam/24/11.jpg" />
 				</div>
 				<div>
-					<img src="onam/24/12.jpg" />
+					<img alt="" src="onam/24/12.jpg" />
 				</div>
 			</div>
 		</div >
